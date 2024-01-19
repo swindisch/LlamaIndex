c17f9b455e4805ce221132b8206a040033a639ef
Sascha Windisch <sascha.windisch@adesso.se>
2024-01-19 09:17:53 +0100

    Refactor code and remove unused imports

--- simple.py
+++ simple.py
@@ -1,20 +1,17 @@
-import os
-from dotenv import load_dotenv, find_dotenv
 import logging
 import sys
+
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
-load_dotenv(find_dotenv(), override=True)
-
+# Logging Level hochsetzen, damit wir alle Meldungen sehen.
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-HUGGINGFACEHUB_API_TOKEN = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
-
+# Erstelle den Index aus den Dokumenten.
 documents = SimpleDirectoryReader("data").load_data()
 index = VectorStoreIndex.from_documents(documents)
 
+# Der Index wurde geladen, nun kann er verwendet werden.
 query_engine = index.as_query_engine()
 response = query_engine.query("Was ist generative KI?")
 print(response)
